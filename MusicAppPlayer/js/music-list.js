460a99e3ccefeda215e550e92c483c0237a225a8
Fenyep <68693335+Fenyep@users.noreply.github.com>
2021-10-26 12:36:32 +0100

    Upload on projet v-1
    Index.html, script.js, styles.css, images and songs added

---
+++ MusicAppPlayer/js/music-list.js
@@ -0,0 +1,38 @@
+let allMusic = [ 
+    {
+        name: "MEC EN OR",
+        artist: "LEFA",
+        img: "music-1",
+        src: "music-1"
+    },
+    {
+        name: "TRIP",
+        artist: "LEFA feat Tayc",
+        img: "music-2",
+        src: "music-2"
+    },
+    {
+        name: "230",
+        artist: "LEFA",
+        img: "music-3",
+        src: "music-3"
+    },
+    {
+        name: "TOUT GACHER",
+        artist: "MHD",
+        img: "music-4",
+        src: "music-4"
+    },
+    {
+        name: "SENTIMENTAL",
+        artist: "DAMSO",
+        img: "music-5",
+        src: "music-5"
+    },
+    {
+        name: "QJS",
+        artist: "Gims (feat. jul sch)",
+        img: "music-6",
+        src: "music-6"
+    },
+]
